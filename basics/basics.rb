73157bf8a54ef824e40afb3a12f1624c82af6f94
Lauren Sylvain <lsylvain1726@gmail.com>
2016-04-10 14:57:41 -0400

    Intro To Programming - The Basics

---
+++ basics/basics.rb
@@ -0,0 +1,46 @@
+# Exercise 1
+puts "Lauren " + "Sylvain"
+puts "----"
+#Exercise 2
+
+puts thousands = 4396 / 1000
+puts hundreds = 4936 % 1000 / 100
+puts tens = 4936 % 100 / 10
+puts ones = 4936 % 10
+puts "-----"
+#Excercise 3
+
+hash = { Remember_The_Titans: 1999, 
+         The_Rock: 2003, 
+         Bridesmaids: 2011, 
+         Creed: 2015 }
+
+puts hash[:Remember_The_Titans]
+puts hash[:The_Rock]  
+puts hash[:Bridesmaids]     
+puts hash[:Creed]  
+
+puts "----"
+
+#Excercise 4
+dates = [1999, 2003, 2011, 2015]
+puts dates[0]
+puts dates[1]
+puts dates[2]
+puts dates[3]
+
+puts "-----"
+
+# Excercise 5
+puts 5 * 4 * 3 * 2 * 1
+puts 6 * 5 * 4 * 3 * 2 * 1
+puts 7 * 6 * 5 * 4 * 3 * 2 * 1
+puts 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+puts "----"
+
+#Excercise 6
+
+puts 1.23 * 1.23
+puts 4.35 * 4.35
+puts 5.6789 * 5.6789
