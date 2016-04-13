5f8de210024955901db8759aed0e7f6370d6c3ab
Lauren Sylvain <lsylvain1726@gmail.com>
2016-04-12 21:48:57 -0400

    Loops

---
+++ loops/questions.rb
@@ -0,0 +1,6 @@
+# Exercise 1
+# This returns [1, 2, 3, 4, 5]
+# It returns the original array because the each method does not mutate the original array
+
+
+
---
+++ loops/while.rb
@@ -0,0 +1,9 @@
+
+greeting = ""
+
+while greeting != "STOP" do
+	puts "Are you ok?"
+	val = gets.chomp
+	puts "Are you sure?"
+	greeting = gets.chomp
+end
